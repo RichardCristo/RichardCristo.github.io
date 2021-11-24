575c1fc5ceb1e5fb2408e547eb63430810f2b945
RichardCristo <82117791+RichardCristo@users.noreply.github.com>
2021-11-23 23:04:44 -0300

    Add files via upload

---
+++ tp_final.sql
@@ -0,0 +1,4 @@
+create database tp_final;
+use tp_final;
+create table pedido(id_pedido int, comida varchar(45), acompañamiento varchar(45), bebida varchar(45), domicilio varchar(45));
+select * from pedido;
